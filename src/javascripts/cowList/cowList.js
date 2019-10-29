d3e2691ec86d4a44e4819c3314bff7a061bf847e
Monica Djunaidi <modjun12@gmail.com>
2019-10-28 19:14:19 -0500

    added promises

--- src/javascripts/cowList/cowList.js
+++ src/javascripts/cowList/cowList.js
@@ -4,15 +4,9 @@ import cowData from '../helpers/cows';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      // gets array of keys in object. Now can do forEach on array
-      Object.keys(demCows).forEach((cowID) => {
-        // inside object get value of key, in this case cowID. '.id' creates key of id and value of cowID
-        demCows[cowID].id = cowID;
-        cows.push(demCows[cowID]);
-      });
-      console.log('It worked!', cows);
+      console.log('cow array from cowList', response);
+
+      // call a domString builder function and pass in cows
     })
     .catch((error) => {
       console.error('shit broke', error);
--- src/javascripts/helpers/cows.js
+++ src/javascripts/helpers/cows.js
@@ -2,7 +2,20 @@ import axios from 'axios';
 
 // const getCows = () => axios.get('https://teamtreehouse.com/monicadjunaidi.json');
 
-const getCows = () => axios.get('../../../../db/cows.json');
+const getCows = () => new Promise((resolve, reject) => {
+  // promise within promise so to wait until data has been modified
+  axios.get('../../../../db/cows.json').then((response) => {
+    const demCows = response.data.cows;
+    const cows = [];
+    // gets array of keys in object. Now can do forEach on array
+    Object.keys(demCows).forEach((cowID) => {
+      // inside object get value of key, in this case cowID. '.id' creates key of id and value of cowID
+      demCows[cowID].id = cowID;
+      cows.push(demCows[cowID]);
+    });
+    resolve(cows);
+  }).catch((error) => reject(error));
+});
 
 
 export default { getCows };
