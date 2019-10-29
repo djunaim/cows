1fbbfa115a64bfd3f515b11205f7fb6bb33671c0
Monica Djunaidi <modjun12@gmail.com>
2019-10-28 19:00:58 -0500

    wip

--- src/javascripts/cowList/cowList.js
+++ src/javascripts/cowList/cowList.js
@@ -1,10 +1,18 @@
 import './cowList.scss';
-import cows from '../helpers/cows';
+import cowData from '../helpers/cows';
 
 const buildCows = () => {
-  cows.getCows()
+  cowData.getCows()
     .then((response) => {
-      console.log('It worked!', response);
+      const demCows = response.data.cows;
+      const cows = [];
+      // gets array of keys in object. Now can do forEach on array
+      Object.keys(demCows).forEach((cowID) => {
+        // inside object get value of key, in this case cowID. '.id' creates key of id and value of cowID
+        demCows[cowID].id = cowID;
+        cows.push(demCows[cowID]);
+      });
+      console.log('It worked!', cows);
     })
     .catch((error) => {
       console.error('shit broke', error);
--- src/javascripts/helpers/cows.js
+++ src/javascripts/helpers/cows.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-const getCows = () => axios.get('https://teamtreehouse.com/monicadjunaidi.json');
+// const getCows = () => axios.get('https://teamtreehouse.com/monicadjunaidi.json');
+
+const getCows = () => axios.get('../../../../db/cows.json');
+
 
 export default { getCows };
